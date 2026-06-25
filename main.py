96fb38608e2376ac3adb8bb76447445c97cd8af4
NORBOYEV SAMARIDDIN FAZLIDDIN O‘G‘LI <s.norboyev@corp.ishonchkredit.uz>
2026-06-25 10:12:03 +0500

    a

--- main.py
+++ main.py
@@ -50,10 +50,10 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="FoodExpress - Notification Service", version="1.0.0", lifespan=lifespan)
 
 origins = [
-    "http://localhost:3000",  
-    "http://127.0.0.1:3000",  
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "*",                      
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",        
 ]
 
 
